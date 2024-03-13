5f538c8844b253f0e263b1e1d20cfbbff8f009c7
giorka <mokenosov@ro.ru>
2024-03-13 23:31:02 +0300

    / Subject Maximum Length

--- server/v1/models.py
+++ server/v1/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class EmailMessage(models.Model):
     type = models.CharField(max_length=32)
-    subject = models.CharField(max_length=32)
+    subject = models.CharField(max_length=64)
     message = models.TextField()
 
     class Meta:
